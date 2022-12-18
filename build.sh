3fc10832574794ef5c0a2d8eeda39fd0b9d7db0a
piyushpanchal114 <piyushpanchal114@gmail.com>
2022-12-18 19:38:15 +0530

    changes in build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+python3.9 pipenv install
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput

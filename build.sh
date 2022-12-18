eec32d189a4abc1b47b4cbf1ec5cdfc343c8c9e2
piyushpanchal114 <piyushpanchal114@gmail.com>
2022-12-18 18:07:44 +0530

    python version changed

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 # Build the project
 echo "Building the project..."
-python3.10.4 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.10.4 manage.py makemigrations --noinput
-python3.10.4 manage.py migrate
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate
 
 echo "Collect Static..."
-python3.10.4 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
 
